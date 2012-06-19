d278d813e49fc95a7a46514bd3021d55fce5053e
Soham Sadhu <sxs9174@rit.edu>
2012-06-19 14:36:29 -0400

    Finish user signup.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ FirstApp::Application.routes.draw do
 
   resources :users
 
+  root to: 'static_pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
